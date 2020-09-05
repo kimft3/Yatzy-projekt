0ef28b633c9209c15c98d7dbe8347351a938544b
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-09-05 19:14:51 +0200

    Wow such work done

--- yatzy.js
+++ yatzy.js
@@ -1,23 +1,234 @@
-let dice = []
+let dice = [1, 2, 3, 4, 5]
+let diceHeld = [false, false, false, false, false]
 let throwCount = 0
 let ones, twos, threes, fours, fives, sixes = 0
 
 function rollDice() {
-for (let i = 0; i < 5; i++) {
-    dice[i] = Math.floor((Math.random()* 6 )+ 1)
+    if (throwCount < 3) {
+        for (let i = 0; i < 5; i++) {
+            if (!diceHeld[i]) {
+                dice[i] = Math.floor((Math.random() * 6) + 1)
+            }
+        }
+        throwCount++
+    } else {
+        gameEndAlert()
     }
-    throwCount++
+    updateFieldsAfterRoll()
 }
 
+function resetGame() {
+    dice = [1, 2, 3, 4, 5]
+    diceHeld = [false, false, false, false, false]
+    throwCount = 0
+}
 
+function frequency() {
+    let freq = [0, 0, 0, 0, 0, 0, 0]
+    for (let i = 1; i < 7; i++) {
+        for (let j = 0; j < dice.length; j++) {
+            if (dice[j] == i) {
+                freq[i]++
+            }
+        }
+    }
+    return freq
+}
 
-// For testing
-rollDice()
+function sameValuePoints(value) {
+    let svPoints = frequency()[value]
+    let points = svPoints * value
+    return points
+}
 
-console.log(dice.toString())
-console.log(throwCount)
+function onePairPoints() {
+    let temp = 0
+    let f = frequency()
+    for (let i = 1; i < f.length; i++) {
+        if (f[i] > 1) {
+            if (temp < i + i) {
+                temp = i + i
+            }
+        }
+    }
+    return temp
+}
 
-rollDice()
+function twoPairPoints() {
+    let value = onePairPoints()
+    let value1 = 0
+    let temp = 0
+    let f = frequency()
+    for (let i = 1; i < f.length; i++) {
+        if (f[i] > 1 && i != value / 2) {
+            if (temp < i + i) {
+                temp = i + i
+            }
+            value1 = value + temp
+        }
+    }
+    return value1
+}
 
-console.log(dice.toString())
-console.log(throwCount)
+function threeSamePoints() {
+    let value = 0
+    let f = frequency()
+    for (let i = 1; i < f.length; i++) {
+        if (f[i] >= 3) {
+            value = i * 3
+        }
+    }
+    return value
+}
+
+function fourSamePoints() {
+    let value = 0
+    let f = frequency()
+    for (let i = 1; i < f.length; i++) {
+        if (f[i] >= 4) {
+            value = i * 4
+        }
+    }
+    return value
+}
+
+function fullHousePoints() {
+    let temp = 0
+    let twoPairs = twoPairPoints()
+    let threeSame = threeSamePoints()
+    if (threeSame > 0 && twoPairs > 0) {
+        temp = twoPairs + (threeSame / 3)
+    }
+    return temp
+}
+
+function smallStraightPoints() {
+    let count = 0
+    let value = 0
+    let f = frequency()
+    for (let i = 1; i <= 5; i++) {
+        if (f[i] == 1) {
+            count++
+        }
+    }
+    if (count == 5) {
+        value = 15
+    }
+    return value
+}
+
+function largeStraightPoints() {
+    let count = 0
+    let value = 0
+    let f = frequency()
+    for (let i = 2; i <= 6; i++) {
+        if (f[i] == 1) {
+            count++
+        }
+    }
+    if (count == 5) {
+        value = 20
+    }
+    return value
+}
+
+function chancePoints() {
+    let value = 0
+    for (let i = 0; 1 < dice.length; i++) {
+        value = value + dice[i]
+    }
+    return value
+}
+
+function yatzyPoints() {
+    let value = 0
+    let f = frequency()
+    for (let i = 1; i < f.length; i++) {
+        if (f[i] >= 5) {
+            value = 50
+        }
+    }
+    return value
+}
+
+function getResults() {
+    let results = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+    for (let i = 0; i <= 5; i++) {
+        results[i] = sameValuePoints(i + 1)
+    }
+    results[6] = onePairPoints()
+    results[7] = twoPairPoints()
+    results[8] = threeSamePoints()
+    results[9] = fourSamePoints()
+    results[10] = fullHousePoints()
+    results[11] = smallStraightPoints()
+    results[12] = largeStraightPoints()
+    results[13] = chancePoints()
+    results[14] = yatzyPoints()
+    return results
+}
+
+function showThrow() {
+    let string = "Turn" + throwCount
+    return string
+}
+
+//---------------------------------------------------------------------------//
+
+const turnCountNode = document.getElementById("turnCounter")
+
+const dieSlots = [document.getElementById("die1"),
+    document.getElementById("die2"),
+    document.getElementById("die3"),
+    document.getElementById("die4"),
+    document.getElementById("die5")
+]
+
+const dieFaces = ["<img src='images/diceone.png' style='height: 40px;'></img>",
+    "<img src='images/dicetwo.png' style='height: 40px;'></img>",
+    "<img src='images/dicethree.png' style='height: 40px;'></img>",
+    "<img src='images/dicefour.png' style='height: 40px;'></img>",
+    "<img src='images/dicefive.png' style='height: 40px;'></img>",
+    "<img src='images/dicesix.png' style='height: 40px;'></img>"
+]
+
+for (let i = 0; i < 5; i++) {
+    dieSlots[i].innerHTML = dieFaces[i]
+}
+
+const resultFields = [document.getElementById('onesText'),
+    document.getElementById('twosText'),
+    document.getElementById('threesText'),
+    document.getElementById('foursText'),
+    document.getElementById('fivesText'),
+    document.getElementById('sixesText')
+]
+
+const rollBtn = document.getElementById("roller")
+rollBtn.addEventListener('click', e => rollDice())
+
+function gameEndAlert() {
+    alert('Spil slut')
+    resetGame()
+    resetGUI()
+}
+
+function resetGUI() {
+    for (let i = 0; i < 6; i++) {
+        resultFields[i].value = 0
+    }
+}
+
+function updateFieldsAfterRoll() {
+    //throwCount
+    turnCountNode.innerText = "Turn " + throwCount
+    //Dice display
+    for (let i = 0; i < 5; i++) {
+        let j = (dice[i] - 1)
+        dieSlots[i].innerHTML = dieFaces[j]
+    }
+    //samevalue (1s, 2s, etc) points
+    for (let i = 0; i < 6; i++) {
+        resultFields[i].value = sameValuePoints(i + 1)
+    }
+}
